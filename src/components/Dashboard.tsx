9244b6a1d9d7b07ec2a0f046ef8a7dbf37db9f63
GarimaGupta40 <ggupta1_be22@thapar.edu>
2026-06-30 13:44:53 +0530

    Update webhook URL

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect, useRef } from "react";
 
 type Screen = "landing" | "loading" | "success";
 
-const WEBHOOK_URL = "https://hook.eu1.make.com/5wwddnrmgsius8tm7c87g07m1spj7d5e";
+const WEBHOOK_URL = "https://hook.eu1.make.com/zjdusb91us9x467vj15twdd0yhfy9v1f";
 
 const STEPS = [
   "Collecting Social Media Data",
